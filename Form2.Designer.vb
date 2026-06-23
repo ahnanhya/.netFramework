385e19f9df5637f73d4b8e095a40c9689badc2df
ahnanhya <24sbca005@psgrkcw.ac.in>
2026-06-23 15:27:15 +0530

    Add files via upload

---
+++ Form2.Designer.vb
@@ -0,0 +1,432 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CollegeApplicationForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        ComboBox2 = New ComboBox()
+        ComboBox1 = New ComboBox()
+        Label4 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Button2 = New Button()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        RichTextBox1 = New RichTextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        Label16 = New Label()
+        Label17 = New Label()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(88, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(169, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "COLLEGE APPLICATION FORM"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(57, 57)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(44, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Degree"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(57, 86)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(44, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Branch"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(492, 242)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 5
+        Button1.Text = "Submit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(153, 83)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 23)
+        ComboBox2.TabIndex = 6
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(153, 54)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 7
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(57, 122)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(55, 15)
+        Label4.TabIndex = 8
+        Label4.Text = "Category"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(153, 120)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(37, 19)
+        RadioButton1.TabIndex = 9
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "SF"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(253, 122)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(56, 19)
+        RadioButton2.TabIndex = 10
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Aided"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(88, 157)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 15)
+        Label5.TabIndex = 11
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(91, 157)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(111, 15)
+        Label6.TabIndex = 12
+        Label6.Text = "PERSONAL DETAILS"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(57, 186)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(39, 15)
+        Label7.TabIndex = 13
+        Label7.Text = "Name"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(57, 214)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(75, 15)
+        Label8.TabIndex = 14
+        Label8.Text = "Father Name"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(57, 246)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(69, 15)
+        Label9.TabIndex = 15
+        Label9.Text = "Occupation"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(57, 282)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(29, 15)
+        Label10.TabIndex = 16
+        Label10.Text = "Dob"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(57, 314)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(45, 15)
+        Label11.TabIndex = 17
+        Label11.Text = "Gender"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(153, 183)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(156, 23)
+        TextBox1.TabIndex = 18
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(153, 214)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(156, 23)
+        TextBox2.TabIndex = 19
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(153, 246)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(156, 23)
+        TextBox3.TabIndex = 20
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(153, 279)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(156, 23)
+        TextBox4.TabIndex = 21
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(492, 274)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 24
+        Button2.Text = "Cancel"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(91, 352)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(155, 15)
+        Label12.TabIndex = 25
+        Label12.Text = "COMMUNICATION DETAILS"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(57, 383)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(49, 15)
+        Label13.TabIndex = 26
+        Label13.Text = "Address"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Location = New Point(59, 445)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(33, 15)
+        Label14.TabIndex = 27
+        Label14.Text = "State"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Location = New Point(57, 488)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(50, 15)
+        Label15.TabIndex = 28
+        Label15.Text = "Country"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(153, 380)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(156, 52)
+        RichTextBox1.TabIndex = 29
+        RichTextBox1.Text = ""
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(153, 442)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(156, 23)
+        TextBox5.TabIndex = 30
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(153, 480)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(156, 23)
+        TextBox6.TabIndex = 31
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(153, 314)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(52, 19)
+        CheckBox1.TabIndex = 32
+        CheckBox1.Text = "Male"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(253, 314)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(64, 19)
+        CheckBox2.TabIndex = 33
+        CheckBox2.Text = "Female"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Location = New Point(57, 525)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(41, 15)
+        Label16.TabIndex = 34
+        Label16.Text = "Phone"
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Location = New Point(59, 555)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(36, 15)
+        Label17.TabIndex = 35
+        Label17.Text = "Email"
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(153, 517)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(156, 23)
+        TextBox7.TabIndex = 36
+        TextBox7.Text = "12"
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(153, 555)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(156, 23)
+        TextBox8.TabIndex = 37
+        ' 
+        ' CollegeApplicationForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1372, 742)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox7)
+        Controls.Add(Label17)
+        Controls.Add(Label16)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(RichTextBox1)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Button2)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Label4)
+        Controls.Add(ComboBox1)
+        Controls.Add(ComboBox2)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "CollegeApplicationForm"
+        Text = "College Application Form"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+End Class
---
+++ Form2.vb
@@ -0,0 +1,56 @@
+﻿Public Class CollegeApplicationForm
+    Private num As String = ""
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Clear() ' Clears default items to prevent duplicates
+        ComboBox1.Items.Add("UG")
+        ComboBox1.Items.Add("PG")
+    End Sub
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedItem Is Nothing Then Exit Sub
+
+        Dim selectedDegree As String = ComboBox1.SelectedItem.ToString()
+
+        If selectedDegree = "UG" Then
+            ComboBox2.Items.Clear()
+            ComboBox2.Items.Add("BCA")
+            ComboBox2.Items.Add("BBA")
+            ComboBox2.Items.Add("BSC")
+        ElseIf selectedDegree = "PG" Then
+            ComboBox2.Items.Clear()
+            ComboBox2.Items.Add("MCA")
+            ComboBox2.Items.Add("MBA")
+            ComboBox2.Items.Add("MSC")
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        num = TextBox7.Text.Trim()
+
+        ' Check if the length is exactly 10
+        If num.Length = 10 Then
+            MessageBox.Show("THANK YOU")
+        Else
+            MessageBox.Show("Invalid! The phone number must be exactly 10 digits long.",
+                            "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox7.Focus()
+        End If
+        If Not TextBox8.Text.EndsWith("@gmail.com") Then
+            MessageBox.Show("Invalid email! You must enter an email address ending with @gmail.com.",
+                            "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox8.Focus()
+            Exit Sub
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            CheckBox2.Checked = False
+        End If
+    End Sub
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked Then
+            CheckBox1.Checked = False
+        End If
+    End Sub
+End Class
