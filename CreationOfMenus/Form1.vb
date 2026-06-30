86d0349a0bf1d7ef96e6d9892de8c7af98e3801b
ahnanhya <24sbca005@psgrkcw.ac.in>
2026-06-30 16:56:50 +0530

    Add Blue color option to menu and fix copy handler

--- CreationOfMenus/Form1.vb
+++ CreationOfMenus/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class Form1
     Private Sub RedToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles RedToolStripMenuItem.Click
@@ -7,14 +7,14 @@ Public Class Form1
     Private Sub GreenToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles GreenToolStripMenuItem.Click
         TextBox1.ForeColor = Color.Green
     End Sub
-    Private Sub YellowToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles YellowToolStripMenuItem.Click
-        TextBox1.ForeColor = Color.Yellow
+    Private Sub BlueToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles BlueToolStripMenuItem.Click
+        TextBox1.ForeColor = Color.Blue
     End Sub
     Private Sub CutToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles CutToolStripMenuItem.Click
         Clipboard.SetText(TextBox1.SelectedText)
         TextBox1.Text = ""
     End Sub
-    Private Sub CopyToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles CopyToolStripMenuItem.Click
+    Private Sub CopyToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles CutToolStripMenuItem.Click
         Clipboard.SetText(TextBox1.SelectedText)
     End Sub
     Private Sub PasteToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles PasteToolStripMenuItem.Click
